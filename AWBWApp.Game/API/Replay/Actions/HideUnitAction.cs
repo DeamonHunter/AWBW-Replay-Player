1eb4f8384eb43e8a97e2ce6c5dd72eafca3f8ee8
DeamonHunter <deamonhunterextra@outlook.com>
2022-02-21 13:06:20 +1000

    Add the Hide and Unhide actions.

---
+++ AWBWApp.Game/API/Replay/Actions/HideUnitAction.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using AWBWApp.Game.Game.Logic;
+using AWBWApp.Game.Helpers;
+using Newtonsoft.Json.Linq;
+using osu.Framework.Logging;
+
+namespace AWBWApp.Game.API.Replay.Actions
+{
+    public class HideUnitActionBuilder : IReplayActionBuilder
+    {
+        public string Code => "Hide";
+
+        public ReplayActionDatabase Database { get; set; }
+
+        public IReplayAction ParseJObjectIntoReplayAction(JObject jObject, ReplayData replayData, TurnData turnData)
+        {
+            var action = new HideUnitAction();
+
+            var moveObj = jObject["Move"];
+
+            if (moveObj is JObject moveData)
+            {
+                var moveAction = Database.ParseJObjectIntoReplayAction(moveData, replayData, turnData);
+                action.MoveUnit = moveAction as MoveUnitAction;
+                if (moveAction == null)
+                    throw new Exception("Capture action was expecting a movement action.");
+            }
+
+            var hideData = (JObject)jObject["Hide"];
+            if (hideData == null)
+                throw new Exception("Hide Replay Action did not contain information about Hide.");
+
+            action.HidingUnitID = (int)ReplayActionHelper.GetPlayerSpecificDataFromJObject((JObject)hideData["unit"], turnData.ActiveTeam, turnData.ActivePlayerID);
+            return action;
+        }
+    }
+
+    public class HideUnitAction : IReplayAction
+    {
+        public int HidingUnitID { get; set; }
+
+        public MoveUnitAction MoveUnit;
+
+        public IEnumerable<ReplayWait> PerformAction(ReplayController controller)
+        {
+            Logger.Log("Performing Hide Action.");
+            Logger.Log("Todo: Play transition effect.");
+
+            if (MoveUnit != null)
+            {
+                foreach (var transformable in MoveUnit.PerformAction(controller))
+                    yield return transformable;
+            }
+
+            var hidingUnit = controller.Map.GetDrawableUnit(HidingUnitID);
+            hidingUnit.Dived.Value = true;
+        }
+
+        public void UndoAction(ReplayController controller, bool immediate)
+        {
+            Logger.Log("Undoing Capture Action.");
+            throw new NotImplementedException("Undo Action for Capture Building is not complete");
+            //controller.Map.DestroyUnit(NewUnit.ID, false, immediate);
+        }
+    }
+}
---
+++ AWBWApp.Game/API/Replay/Actions/UnhideUnitAction.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using AWBWApp.Game.Game.Logic;
+using AWBWApp.Game.Helpers;
+using Newtonsoft.Json.Linq;
+using osu.Framework.Logging;
+
+namespace AWBWApp.Game.API.Replay.Actions
+{
+    public class UnhideUnitActionBuilder : IReplayActionBuilder
+    {
+        public string Code => "Unhide";
+
+        public ReplayActionDatabase Database { get; set; }
+
+        public IReplayAction ParseJObjectIntoReplayAction(JObject jObject, ReplayData replayData, TurnData turnData)
+        {
+            var action = new UnhideUnitAction();
+
+            var moveObj = jObject["Move"];
+
+            if (moveObj is JObject moveData)
+            {
+                var moveAction = Database.ParseJObjectIntoReplayAction(moveData, replayData, turnData);
+                action.MoveUnit = moveAction as MoveUnitAction;
+                if (moveAction == null)
+                    throw new Exception("Unhide action was expecting a movement action.");
+            }
+
+            var hideData = (JObject)jObject["Unhide"];
+            if (hideData == null)
+                throw new Exception("Unhide Replay Action did not contain information about Unhide.");
+
+            action.RevealingUnit = ReplayActionHelper.ParseJObjectIntoReplayUnit((JObject)ReplayActionHelper.GetPlayerSpecificDataFromJObject((JObject)hideData["unit"], turnData.ActiveTeam, turnData.ActivePlayerID));
+            return action;
+        }
+    }
+
+    public class UnhideUnitAction : IReplayAction
+    {
+        public ReplayUnit RevealingUnit { get; set; }
+
+        public MoveUnitAction MoveUnit;
+
+        public IEnumerable<ReplayWait> PerformAction(ReplayController controller)
+        {
+            Logger.Log("Performing Unhide Action.");
+            Logger.Log("Todo: Play transition effect.");
+
+            if (MoveUnit != null)
+            {
+                foreach (var transformable in MoveUnit.PerformAction(controller))
+                    yield return transformable;
+            }
+
+            var hiddenUnit = controller.Map.GetDrawableUnit(RevealingUnit.ID);
+            hiddenUnit.UpdateUnit(RevealingUnit);
+        }
+
+        public void UndoAction(ReplayController controller, bool immediate)
+        {
+            Logger.Log("Undoing Capture Action.");
+            throw new NotImplementedException("Undo Action for Capture Building is not complete");
+            //controller.Map.DestroyUnit(NewUnit.ID, false, immediate);
+        }
+    }
+}
