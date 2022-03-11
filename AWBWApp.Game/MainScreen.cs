ccb3479eb0f8e3fedbf25d301fdd0151efb5db50
DeamonHunter <deamonhunterextra@outlook.com>
2022-03-11 21:20:16 +1000

    Make screens transition between each other.

--- AWBWApp.Game/MainScreen.cs
+++ AWBWApp.Game/MainScreen.cs
@@ -115,6 +115,7 @@ namespace AWBWApp.Game
         public override void OnEntering(IScreen last)
         {
             buttonsContainer.FadeOut();
+            this.FadeIn();
             logo.Animate();
         }
 
--- AWBWApp.Game/UI/EscapeableScreen.cs
+++ AWBWApp.Game/UI/EscapeableScreen.cs
@@ -1,4 +1,5 @@
-﻿using osu.Framework.Input.Events;
+﻿using osu.Framework.Graphics;
+using osu.Framework.Input.Events;
 using osu.Framework.Screens;
 using osuTK.Input;
 
@@ -15,6 +16,30 @@ namespace AWBWApp.Game.UI
             }
 
             return base.OnKeyDown(e);
+        }
+
+        public override void OnEntering(IScreen last)
+        {
+            this.FadeInFromZero(250, Easing.OutQuint);
+            base.OnEntering(last);
+        }
+
+        public override void OnResuming(IScreen last)
+        {
+            this.FadeIn(250, Easing.OutQuint);
+            base.OnResuming(last);
+        }
+
+        public override void OnSuspending(IScreen next)
+        {
+            this.FadeOut(250, Easing.In);
+            base.OnSuspending(next);
+        }
+
+        public override bool OnExiting(IScreen next)
+        {
+            this.FadeOut(125, Easing.Out);
+            return base.OnExiting(next);
         }
     }
 }
