bfe01be03f87e623fd591c6a63dfbe6bcbfea75b
Justus Lind <DeamonHunter@users.noreply.github.com>
2022-08-06 16:24:17 +1000

    Fix deleting units not counting after a played action.

--- AWBWApp.Game/API/Replay/Actions/DeleteUnitAction.cs
+++ AWBWApp.Game/API/Replay/Actions/DeleteUnitAction.cs
@@ -83,6 +83,7 @@ namespace AWBWApp.Game.API.Replay.Actions
             }
 
             controller.Map.DeleteUnit(DeletedUnitId, true);
+            ReplayActionHelper.AdjustStatsToPlayerAction(controller, originalUnits[0].PlayerID!.Value, originalUnits.Values, false);
             controller.ActivePlayer.UnitValue.Value -= unitValue;
             controller.UpdateFogOfWar();
         }
@@ -93,6 +94,7 @@ namespace AWBWApp.Game.API.Replay.Actions
             foreach (var unit in originalUnits)
                 controller.Map.AddUnit(unit.Value);
 
+            ReplayActionHelper.AdjustStatsToPlayerAction(controller, originalUnits[0].PlayerID!.Value, originalUnits.Values, true);
             controller.ActivePlayer.UnitValue.Value += unitValue;
 
             controller.UpdateFogOfWar();
