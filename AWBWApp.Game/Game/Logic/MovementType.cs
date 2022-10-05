f205acaeedd57d91dc6490a83428aa43e0389104
Justus Lind <DeamonHunter@users.noreply.github.com>
2022-10-05 15:08:31 +1000

    Fix MovementType enum not using individual bits and mucking up bitwise math.

--- AWBWApp.Game/Game/Logic/MovementType.cs
+++ AWBWApp.Game/Game/Logic/MovementType.cs
@@ -7,13 +7,13 @@ namespace AWBWApp.Game.Game.Logic
     public enum MovementType
     {
         None = 0,
-        LightInf = 1,
-        HeavyInf = 2,
-        Tire = 3,
-        Tread = 4,
-        Sea = 5,
-        Lander = 6,
-        Air = 7,
-        Pipe = 8
+        LightInf = 1 << 0,
+        HeavyInf = 1 << 1,
+        Tire = 1 << 2,
+        Tread = 1 << 3,
+        Sea = 1 << 4,
+        Lander = 1 << 5,
+        Air = 1 << 6,
+        Pipe = 1 << 7
     }
 }
