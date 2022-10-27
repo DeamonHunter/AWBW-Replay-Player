8e748a4dc4066e83e96d65ba31bb1cca1ca1a642
Justus Lind <DeamonHunter@users.noreply.github.com>
2022-10-27 13:52:26 +1000

    Fix Power actions not applying movement ranges and attack range increases. Fixes #116.

--- AWBWApp.Game/API/Replay/Actions/PowerAction.cs
+++ AWBWApp.Game/API/Replay/Actions/PowerAction.cs
@@ -637,11 +637,14 @@ namespace AWBWApp.Game.API.Replay.Actions
                         if (change.Value.UnitsMoved.HasValue)
                             unit.CanMove.Value = change.Value.UnitsMoved.Value == 0;
 
-                        if (change.Value.MovementPoints.HasValue)
-                            Logger.Log("Unit Movement Change not implemented yet.");
-                        if (change.Value.Range.HasValue)
-                            Logger.Log("Unit Range Change not implemented yet.");
+                        if (change.Value.MovementPoints.HasValue && MovementRangeIncrease == 0)
+                            unit.MovementRange.Value = change.Value.MovementPoints.Value;
 
+                        if (change.Value.Range.HasValue)
+                        {
+                            if (COPower.PowerIncreases == null || !COPower.PowerIncreases.Exists(x => x.AffectedUnits.Contains("all") || x.AffectedUnits.Contains(unit.UnitData.Name)))
+                                unit.AttackRange.Value = new Vector2I(unit.AttackRange.Value.X, change.Value.Range.Value);
+                        }
                         if (unit.HealthPoints.Value <= 0)
                             controller.Map.DeleteUnit(unit.UnitID, true);
                         else
